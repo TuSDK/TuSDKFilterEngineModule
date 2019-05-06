a39570de831cd6263da2d5f2a0a43eade0ab4e20
SeoHys <hu810809002@gmail.com>
2019-05-06 17:39:05 +0800

    子模块初始化

---
+++ src/main/java/org/lasque/tusdkdemohelper/tusdk/TabViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package org.lasque.tusdkdemohelper.tusdk;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * 贴纸ViewPager滑动页
+ * @author xujie
+ * @Date 2018/9/21
+ */
+
+public class TabViewPagerAdapter extends FragmentPagerAdapter {
+
+    // 默认0 其他为贴纸ID
+    public static long mStickerGroupId;
+
+    private List<StickerFragment> mFragments;
+
+    public TabViewPagerAdapter(FragmentManager fm, List<StickerFragment> fragments) {
+        super(fm);
+        mFragments = fragments;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        if(object instanceof StickerFragment){
+            ((StickerFragment) object).refetchStickerList();
+            return POSITION_NONE;
+        }
+        return super.getItemPosition(object);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public StickerFragment getItem(int i) {
+        return mFragments.get(i);
+    }
+}
